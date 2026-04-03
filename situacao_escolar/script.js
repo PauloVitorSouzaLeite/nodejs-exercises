f11851e4b5cfe03fd310d98d9172b45e7a81e0ff
Paulo Vitor Souza Leite <vitorsousaleite86@gmail.com>
2026-04-03 13:25:51 -0300

    Update script.js

--- situacao_escolar/script.js
+++ situacao_escolar/script.js
@@ -1,7 +1,13 @@
 function situacaoaluno(nota1 = 0, nota2 = 0, nota3 = 0) {
     let media = (nota1 + nota2 + nota3) / 3
-    return media >= 7 ? "O aluno está aprovado!" :
-        media >= 5 ? "O aluno está de recuperação!" :
-        "O aluno está reprovado!"
+    let situacao
+    if (media >= 7) {
+        situacao = "O aluno está aprovado!"
+    } else if (media >= 5) {
+        situacao = "O aluno está em recuperação!"
+    } else {
+        situacao = "O aluno está reprovado!"
+    }
+    return situacao
 }
-console.log(situacaoaluno(5,10,6))
+console.log(situacaoaluno(7,4,2))
